3b9040db4dee87a4b10f0bfe5b4bb478942963ed
SagarDutta054 <sagarduttadesktop2023@gmail.com>
2026-04-28 13:46:04 +0530

    Updated description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -10,6 +10,11 @@ function Description() {
         <p>Example: 2! = 2*1!</p>
         <p>Example: 1! = 1</p>
         <h4>Finally, 4! = 4*3*2*1 = 24</h4>
+        <br />
+        <h3>Important note:</h3>
+        <h4>In this project the consistent Input range is 0-170</h4>
+        <h4>For input &gt; 170 it shows infinity</h4>
+      
       </div>
     </>
   );
